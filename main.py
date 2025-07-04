57f3344a613abc2874d1195f71c4c1ce09524acb
Daniel Dawson Jensen <dadj@aarhus.dk>
2025-07-04 13:53:07 +0200

    updated to test automation server

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import asyncio
 import logging
 import sys
+import os
+
+import requests
 
 from automation_server_client import AutomationServer, Workqueue, WorkItemError
 
 
 async def populate_queue(workqueue: Workqueue):
-    logger = logging.getLogger(__name__)
+    webforms = [
+        "spoergeskema_hypnoterapi_foer_fo",
+        "basisteam_spoergeskema_til_fagpe",
+        # ...add all 6
+    ]
 
-    logger.info("Hello from populate workqueue!")
+    for form_id in webforms:
+        workqueue.add_item(
+            data={"os2_webform_id": form_id},
+            reference=form_id  # can be anything unique
+        )
+
+    print("HELLO WORLD")
 
 
 async def process_workqueue(workqueue: Workqueue):
@@ -23,10 +36,15 @@ async def process_workqueue(workqueue: Workqueue):
             try:
                 # Process the item here
                 pass
+
             except WorkItemError as e:
                 # A WorkItemError represents a soft error that indicates the item should be passed to manual processing or a business logic fault
                 logger.error(f"Error processing item: {data}. Error: {e}")
+
                 item.fail(str(e))
+
+
+print("TEST PRINT")
 
 
 if __name__ == "__main__":
@@ -39,7 +57,9 @@ if __name__ == "__main__":
     # Queue management
     if "--queue" in sys.argv:
         workqueue.clear_workqueue("new")
+
         asyncio.run(populate_queue(workqueue))
+
         exit(0)
 
     # Process workqueue
