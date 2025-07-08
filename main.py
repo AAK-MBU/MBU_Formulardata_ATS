51264e751fce8432a8f824fac8443a48221e96ee
Daniel Dawson Jensen <dadj@aarhus.dk>
2025-07-08 12:08:41 +0200

    update main

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from automation_server_client import AutomationServer, Workqueue, WorkItemError
 
 
 async def populate_queue(workqueue: Workqueue):
+    """
+    Function to populate the workqueue with items.
+    """
+
     webforms = [
         "spoergeskema_hypnoterapi_foer_fo",
         "basisteam_spoergeskema_til_fagpe",
@@ -22,6 +26,10 @@ async def populate_queue(workqueue: Workqueue):
 
 
 async def process_workqueue(workqueue: Workqueue):
+    """
+    Function to process the workqueue items.
+    """
+
     logger = logging.getLogger(__name__)
 
     logger.info("Hello from process workqueue!")
@@ -36,6 +44,7 @@ async def process_workqueue(workqueue: Workqueue):
 
             except WorkItemError as e:
                 # A WorkItemError represents a soft error that indicates the item should be passed to manual processing or a business logic fault
+                # pylint: disable=W1203
                 logger.error(f"Error processing item: {data}. Error: {e}")
 
                 item.fail(str(e))
@@ -47,17 +56,17 @@ print("TEST PRINT")
 if __name__ == "__main__":
     ats = AutomationServer.from_environment()
 
-    workqueue = ats.workqueue()
+    test_workqueue = ats.workqueue()
 
     # Initialize external systems for automation here..
 
     # Queue management
     if "--queue" in sys.argv:
-        workqueue.clear_workqueue("new")
+        test_workqueue.clear_workqueue("new")
 
-        asyncio.run(populate_queue(workqueue))
+        asyncio.run(populate_queue(test_workqueue))
 
         exit(0)
 
     # Process workqueue
-    asyncio.run(process_workqueue(workqueue))
+    asyncio.run(process_workqueue(test_workqueue))
